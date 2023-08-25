dd6ae5b5be13772fa2834a84d54c5e96061fedbc
kunalsupekar <118017473+kunalsupekar@users.noreply.github.com>
2023-08-26 01:06:56 +0530

    payment

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -7,7 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import Transaction from '../Dashboard_components/Transaction';
 // import RazorpayForm from '../ponents/RazorpayForm';
 import LoanForm from '../Dashboard_components/LoanForm';
-import Pay from './Pay';
+import Deposit from '../Dashboard_components/Deposit';
 
 
 const Dashboard = ({user}) => {
@@ -21,7 +21,7 @@ const Dashboard = ({user}) => {
      <Routes>
      <Route path="/transfer" element={<Transfer />} />
      <Route path="/transaction" element={<Transaction/>} />
-     <Route path="/deposit" element={<Pay/>} />
+     <Route path="/deposit" element={<Deposit/>} />
      <Route path="/loan" element={<LoanForm/>} />
      </Routes>
 
