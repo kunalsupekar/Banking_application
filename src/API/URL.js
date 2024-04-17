225bbe9bfd167835d642196320236694a622814c
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2024-04-17 23:24:32 +0530

    finaldfss

--- src/API/URL.js
+++ src/API/URL.js
@@ -1,7 +1,7 @@
 
  // const   base_url1="https://kunalbank.onrender.com"
 //const  base_url1="http://localhost:8090";
-const base_url1="http://ec2-52-66-195-2.ap-south-1.compute.amazonaws.com";
+const base_url1="https://ec2-52-66-195-2.ap-south-1.compute.amazonaws.com";
 
 
 export default base_url1;
