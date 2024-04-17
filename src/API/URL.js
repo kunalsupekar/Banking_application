529096437d597ed85ba5ecbaabce20972f4550cf
kunalsupekar <kunal.supekar@mitaoe.ac.in>
2024-04-17 23:15:17 +0530

    final

--- src/API/URL.js
+++ src/API/URL.js
@@ -1,6 +1,7 @@
 
  // const   base_url1="https://kunalbank.onrender.com"
-const  base_url1="http://localhost:8090";
+//const  base_url1="http://localhost:8090";
+const base_url1="http://52.66.195.2";
 
 
 export default base_url1;
